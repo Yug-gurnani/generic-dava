980370208ed5cfe8da7ad43e775c596d2efc255f
Yug-gurnani <yug.gurnani091@gmail.com>
2022-09-26 19:40:03 +0530

    Skipped authenticity token verification in signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,7 @@
 
 module Users
   class RegistrationsController < Devise::RegistrationsController
+    skip_before_action :verify_authenticity_token
     respond_to :json
 
     private
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 module Users
   class SessionsController < Devise::SessionsController
+    skip_before_action :verify_authenticity_token
     respond_to :json
 
     private
