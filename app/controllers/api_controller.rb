f3924c365e3ac6c47b852ca8fc5cd323ecf1e094
Yug-gurnani <yug.gurnani091@gmail.com>
2022-09-26 18:48:18 +0530

    Removed CSRF token

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,4 @@
 class ApiController < ActionController::API
-  protect_from_forgery with: :null_session
   include ApiRenderConcern
   before_action :authenticate_user!
   before_action :check_verified_user, except: :verify_user
